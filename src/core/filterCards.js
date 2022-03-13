28b05e6a8e5af74032d4eeae7e6f7e3c94c8dbf7
gOHostwith1R <roslik2014@gmail.com>
2022-03-13 21:19:16 +0300

    delete core

--- src/core/filterCards.js
+++
@@ -1,8 +0,0 @@
-import { drawCards } from "./drawCards.js";
-import {cardsData} from "../../mock/mockCards.js";
-
-export const filterCards = (event) => {
-    const filterCardsData = cardsData.filter((elem) => elem.title.toLowerCase().includes(event?.target.value.toLowerCase()));
-    const resultDraw = drawCards(filterCardsData);
-    return resultDraw === '' ? `<h1 class="not-found">No results</h1>` : resultDraw;
-}
--- src/core/openBurgerMenu.js
+++
@@ -1,5 +0,0 @@
-const burgerWrapper = document.querySelector('.wrapper-hamburger__menu');
-
-export const openBurgerMenu = () => {
-    burgerWrapper.classList.add('hamburger__visibility');
-}
